01cf1a4406b518a6fda71332ee15e627e2e5e03a
Siddu1879 <105603547+Siddu1879@users.noreply.github.com>
2023-10-29 19:50:35 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,10 +10,10 @@ function Project() {
                     <div class="card px-5 bg-light">
                         <div class="card-img mt-2"><House size={75} /></div>
                         <div class="card-body">
-                            <h3 className="card-title">Hostel World </h3>
+                            <h3 className="card-title">Hospital Page </h3>
                             <br></br>
                             <br></br>
-                            <p className="card-text">Built a fully responsive website using HTML, CSS , JavaScript, PHP for a college hostel which can accept responses and stores it in database.</p>
+                            <p className="card-text">Built a fully responsive website using HTML, CSS , JavaScript, PHP for a Hospital which can accept responses and stores it in database.</p>
                         </div>
                     </div>
                 </div>
@@ -30,8 +30,11 @@ function Project() {
                     <div class="card px-5 bg-light">
                         <div class="card-img mt-2"><Image size={75} /></div>
                         <div class="card-body">
-                            <h3 className="card-title">Flying Object classification using Deep Learning </h3>
-                            <p className="card-text">Built a model with a team of three members, which will classify flying objects by using CNN architectures like VGG16,ResNet,GoogleNet etc</p>
+                            <h3 className="card-title">Health Care Chatbot using Machine learning Techniques </h3>
+                            <p className="card-text">Built a model with a team of three members,create a conversational agent that can engage in 
+meaningful conversations, exhibiting a high level of understanding, 
+empathy, and accuracy, while continuously learning from user 
+interactions to improve its performance over time using feed forward neural network,LSTM,BiLSTM  </p>
                         </div>
                     </div>
                 </div>
